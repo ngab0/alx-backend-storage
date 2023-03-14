e864c8527fa3c6f078c4ad2b04720f2e9485ec16
ngab0 <jackdonnily@gmail.com>
2023-03-14 16:27:51 +0200

    web basics

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-""" Implementing an expiring web cache and tracker
-    obtain the HTML content of a particular URL and returns it """
+""" 
+Implementing an expiring web cache and tracker
+obtain the HTML content of a particular URL and returns it
+"""
 import redis
 import requests
 r = redis.Redis()
@@ -8,9 +10,11 @@ count = 0
 
 
 def get_page(url: str) -> str:
-    """ track how many times a particular URL was accessed in the key
-        "count:{url}"
-        and cache the result with an expiration time of 10 seconds """
+    """ 
+    track how many times a particular URL was accessed in the key
+    "count:{url}"
+    and cache the result with an expiration time of 10 seconds
+    """
     r.set(f"cached:{url}", count)
     resp = requests.get(url)
     r.incr(f"count:{url}")
